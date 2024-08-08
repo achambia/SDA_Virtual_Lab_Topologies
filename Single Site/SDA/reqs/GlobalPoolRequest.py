f99429e99de9f20a169ac803930822b6fe4bb1c9
achambia <104783812+achambia@users.noreply.github.com>
2024-08-08 20:14:31 +0530

    Update GlobalPoolRequest.py

--- Single Site/SDA/reqs/GlobalPoolRequest.py
+++ Single Site/SDA/reqs/GlobalPoolRequest.py
@@ -27,7 +27,7 @@ __license__ = "Cisco Sample Code License, Version 1.1"
 
 """ Catalyst Center request for creating/updating a global IP pool """
 
-from models.IPPoolSettingsModel import IPPoolSettingsModel
+from SDA.models.IPPoolSettingsModel import IPPoolSettingsModel
 
 class GlobalPoolRequest:
     """
