2d353d61ea7de4e31e1c69117b090002353fc91b
achambia <104783812+achambia@users.noreply.github.com>
2024-08-12 18:05:40 +0530

    Create assigndevicerequest.py

---
+++ Single Site/SDA/reqs/assigndevicerequest.py
@@ -0,0 +1,12 @@
+class assigndevicerequest:
+    """
+    API request for creating Discovery
+    """
+    def __init__(self) -> None:
+        """
+        Constructor
+        :param type: Type of site to create
+        :return: none
+        """
+
+        self.device:list
