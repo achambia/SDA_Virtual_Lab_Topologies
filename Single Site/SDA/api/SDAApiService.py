a85e3e098d7887a6f0d52496d7fea51da7ed4de3
achambia <104783812+achambia@users.noreply.github.com>
2024-08-12 19:42:52 +0530

    Update SDAApiService.py

--- Single Site/SDA/api/SDAApiService.py
+++ Single Site/SDA/api/SDAApiService.py
@@ -44,7 +44,7 @@ class SDAApiService(ApiService):
         """
         super().__init__(url, token)
 
-    def addFabricSites(self, addFabricSitesRequestList:list[FabricSitesRequest]) -> dict:
+    def addFabricSites(self, addFabricSitesRequestList:FabricSitesRequest) -> dict:
         """
         Add Fabric Site API
         :param addFabricSitesRequestList: List of 'FabricSitesRequest' objects containing information for adding fabric sites
