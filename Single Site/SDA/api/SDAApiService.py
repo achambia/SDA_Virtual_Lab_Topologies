a825387f963a678c059ae4b6d45301abc8fade69
achambia <104783812+achambia@users.noreply.github.com>
2024-08-12 20:40:36 +0530

    Update SDAApiService.py

--- Single Site/SDA/api/SDAApiService.py
+++ Single Site/SDA/api/SDAApiService.py
@@ -44,13 +44,13 @@ class SDAApiService(ApiService):
         """
         super().__init__(url, token)
 
-    def addFabricSites(self, addFabricSitesRequestList):
+    def addFabricSites(self, addFabricSitesRequestList:FabricSitesRequest):
         """
         Add Fabric Site API
         :param addFabricSitesRequestList: List of 'FabricSitesRequest' objects containing information for adding fabric sites
         :return: API response
         """
-        return self.post('/dna/intent/api/v1/business/sda/fabric', addFabricSitesRequestList)
+        return self.post('/dna/intent/api/v1/business/sda/fabric-site', addFabricSitesRequestList)
 
     def deleteFabricSiteByName(self, siteNameHierarchy:str=None) -> dict:
         """
