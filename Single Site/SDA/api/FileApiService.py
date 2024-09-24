efe89faa40383abb72b7875cb8040027c3780019
achambia <104783812+achambia@users.noreply.github.com>
2024-09-24 11:44:04 +0530

    Create FileApiService.py

---
+++ Single Site/SDA/api/FileApiService.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+Copyright (c) 2019 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+"""
+
+__author__ = "Atul Trasi"
+__email__ = "atrasi@cisco.com"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2019 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+""" Catalyst Center File REST API """
+
+from SDA.SDA.api.ApiService import ApiService
+
+class FileApiService(ApiService):
+    """
+    File API
+    """
+    def __init__(self, url:str, token:str) -> None:
+        """
+        Constructor
+        :param url: Catalyst Center URL
+        :param token: Authentication token
+        :return: none
+        """
+        super().__init__(url, token)
+
+    def downloadFile(self, fileId:str) -> dict:
+        """
+        Download File API
+        :param fileId: File ID
+        :return: File content
+        """
+        return self.get('/dna/intent/api/v1/file/' + fileId)
