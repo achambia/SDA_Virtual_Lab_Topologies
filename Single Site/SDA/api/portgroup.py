4c327345e4ebe53ef2f9a862e9f82e31c2947b5b
achambia <104783812+achambia@users.noreply.github.com>
2024-09-25 14:50:46 +0530

    Create portgroup.py

---
+++ Single Site/SDA/api/portgroup.py
@@ -0,0 +1,16 @@
+import re
+
+from pyVmomi import vim
+from api import service_instance,pchelper
+
+
+def portgroup(host,user,password,port,dvs,pgkey):
+    pg_dict = []
+    si = service_instance.connect(host,user,password,port)
+    content = si.RetrieveContent()
+    portgroup_n = pchelper.get_all_obj(content,[vim.Network])
+
+    for x in portgroup_n:
+        if re.search(f'vim.dvs.DistributedVirtualPortgroup:DVPG-{dvs}-{pgkey}',str(x)) or re.search(f'vim.dvs.DistributedVirtualPortgroup:{pgkey}',str(x)):
+            pg_dict = (portgroup_n[x])
+    return pg_dict
