de13d57ac5b590a9219e37821051a560a9d0ed88
achambia <104783812+achambia@users.noreply.github.com>
2024-11-11 18:24:52 +0530

    Update deploy_ovf.py

--- Single Site/SDA/api/deploy_ovf.py
+++ Single Site/SDA/api/deploy_ovf.py
@@ -1,7 +1,45 @@
+import re
 import subprocess
 import os
 import sys
+import logging
 
+from paramiko.agent import value
+
+logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            handlers=[
+                logging.FileHandler('/home/cisco/app.log'),  # Log messages will be written to 'app.log'
+            ]
+        )
+
+logger = logging.getLogger(__name__)
+
+
+def run_subprocess_and_log(command):
+    """
+    Run a subprocess and log its output in real-time.
+
+    :param command: List of command arguments to execute.
+    """
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,shell=True)
+
+    # Log stdout
+    for stdout_line in iter(process.stdout.readline, ""):
+        logger.info(stdout_line.strip())
+        if re.search('.*Task.*|.*Disk progress.*|.*Powering.*|.*Transfer.*',stdout_line.strip()):
+            print(stdout_line.strip())
+
+    # Log stderr
+    for stderr_line in iter(process.stderr.readline, ""):
+        logger.error(stderr_line.strip())
+
+    process.stdout.close()
+    process.stderr.close()
+    process.wait()
+
+    return process.returncode
 
 def deploy_cml(datastore,vm_name,user,password,network1,network2,host):
     '''
@@ -14,25 +52,26 @@ def deploy_cml(datastore,vm_name,user,password,network1,network2,host):
     :param host: Fqdn or IP address of ESXI
     :return: Returns None.
     '''
+
     try:
-        
+        print('\n!! Initiated the deployment of CML VM !!\n')
         if os.name == 'posix':
             current_dir = os.getcwd()
             os.chdir ('/usr/bin/ovftool')
-            subprocess.run(
-            f'/usr/bin/ovftool/ovftool --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"CML={network1}" /home/cisco/Desktop/Lab_Build/cml/CML.ovf vi://{user}:{password}@{host}/',
-            shell=True)
+            return_code = run_subprocess_and_log(f'/usr/bin/ovftool/ovftool --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"CML={network1}" /home/cisco/Desktop/Lab_Build/cml/CML.ovf vi://{user}:{password}@{host}/')
             os.chdir(current_dir)
+
             
         elif os.name == 'nt':
             current_dir = os.getcwd()
             os.chdir(r'C:\Program Files\VMware\VMware OVF Tool') 
-            subprocess.run(
-            f'ovftool.exe --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"CML={network1}" C:/Users/admin/Desktop/Lab_Build/cml/CML.ovf vi://{user}:{password}@{host}/',
-            shell=True)
+            return_code = run_subprocess_and_log(
+            f'ovftool.exe --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"CML={network1}" C:/Users/admin/Desktop/Lab_Build/cml/CML.ovf vi://{user}:{password}@{host}/')
             os.chdir(current_dir)
+
     except IOError as io_error:
-        print(io_error)
+        logger.error(io_error)
+        print('!! Deployment of CML failed !!\n')
         sys.exit()
     return None
 
@@ -50,20 +89,19 @@ def deploy_ise(datastore,vm_name,network1,user,password,host):
         if os.name == 'posix':
             current_dir = os.getcwd()
             os.chdir ('/usr/bin/ovftool')
-            subprocess.run(
-            f'/usr/bin/ovftool/ovftool --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"Datacenter={network1}" /home/cisco/Desktop/Lab_Build/ISE/ISE.ovf vi://{user}:{password}@{host}/',
-            shell=True)
+            run_subprocess_and_log(f'/usr/bin/ovftool/ovftool --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"Datacenter={network1}" /home/cisco/Desktop/Lab_Build/ISE/ISE.ovf vi://{user}:{password}@{host}/')
             os.chdir(current_dir)
+
+
         elif os.name == 'nt':
             current_dir = os.getcwd()
             os.chdir(r'C:\Program Files\VMware\VMware OVF Tool')
-            subprocess.run(
-            f'ovftool.exe --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"Datacenter={network1}" C:/Users/admin/Desktop/Lab_Build/ise/ise.ovf vi://{user}:{password}@{host}/',
-            shell=True)
+            run_subprocess_and_log(f'ovftool.exe --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"Datacenter={network1}" C:/Users/admin/Desktop/Lab_Build/ise/ise.ovf vi://{user}:{password}@{host}/')
             os.chdir(current_dir)
+
         
     except IOError as io_error:
-        print(io_error)
+        logger.error(io_error)
         sys.exit()
     return None
 
@@ -83,20 +121,18 @@ def deploy_wlc(datastore,vm_name,network1,user,password,host):
         if os.name == 'posix':
             current_dir = os.getcwd()
             os.chdir ('/usr/bin/ovftool')
-            subprocess.run(
-            f'/usr/bin/ovftool/ovftool --powerOn -ds={datastore} -n={vm_name} --noSSLVerify --disableVerification --net:"Datacenter={network1}" /home/cisco/Desktop/Lab_Build/wlc/WLC.ovf vi://{user}:{password}@{host}/',
-            shell=True)
+            run_subprocess_and_log(
+            f'/usr/bin/ovftool/ovftool --powerOn -ds={datastore} -n={vm_name} --noSSLVerify --disableVerification --net:"Datacenter={network1}" /home/cisco/Desktop/Lab_Build/wlc/WLC.ovf vi://{user}:{password}@{host}/')
             os.chdir(current_dir)
         elif os.name == 'nt':
             current_dir = os.getcwd()
             os.chdir(r'C:\Program Files\VMware\VMware OVF Tool')
-            subprocess.run(
-            f'ovftool.exe --powerOn -ds={datastore} -n={vm_name} --noSSLVerify --disableVerification --net:"Datacenter={network1}" C:/Users/admin/Desktop/Lab_Build/wlc/WLC.ovf vi://{user}:{password}@{host}/',
-            shell=True)
+            run_subprocess_and_log(
+            f'ovftool.exe --powerOn -ds={datastore} -n={vm_name} --noSSLVerify --disableVerification --net:"Datacenter={network1}" C:/Users/admin/Desktop/Lab_Build/wlc/WLC.ovf vi://{user}:{password}@{host}/')
             os.chdir(current_dir)
         
     except IOError as io_error:
-        print(io_error)
+        logger.info(io_error)
         sys.exit()
     return None
 
@@ -118,20 +154,18 @@ def deploy_labrouter(datastore,vm_name,network1,network2,network3,user,password,
         if os.name == 'posix':
             current_dir = os.getcwd()
             os.chdir ('/usr/bin/ovftool')
-            subprocess.run(
-            f'/usr/bin/ovftool/ovftool --powerOn -ds={datastore} -n={vm_name} --noSSLVerify --disableVerification --net:"Datacenter={network2}" --net:"VM Network={network1}" --net:"CML={network3}" /home/cisco/Desktop/Lab_Build/labrouter/LABRTR.ovf vi://{user}:{password}@{host}/',
-            shell=True)
+            run_subprocess_and_log(
+            f'/usr/bin/ovftool/ovftool --powerOn -ds={datastore} -n={vm_name} --noSSLVerify --disableVerification --net:"Datacenter={network2}" --net:"VM Network={network1}" --net:"CML={network3}" /home/cisco/Desktop/Lab_Build/labrouter/LABRTR.ovf vi://{user}:{password}@{host}/')
             os.chdir(current_dir)
         elif os.name == 'nt':
             current_dir = os.getcwd()
             os.chdir(r'C:\Program Files\VMware\VMware OVF Tool')
-            subprocess.run(
-            f'ovftool.exe --powerOn -ds={datastore} -n={vm_name} --noSSLVerify --disableVerification --net:"Datacenter={network2}" --net:"VM Network={network1}" --net:"CML={network3}" C:/Users/admin/Desktop/Lab_Build/labrouter/LABRTR.ovf vi://{user}:{password}@{host}/',
-            shell=True)
+            run_subprocess_and_log(
+            f'ovftool.exe --powerOn -ds={datastore} -n={vm_name} --noSSLVerify --disableVerification --net:"Datacenter={network2}" --net:"VM Network={network1}" --net:"CML={network3}" C:/Users/admin/Desktop/Lab_Build/labrouter/LABRTR.ovf vi://{user}:{password}@{host}/')
             os.chdir(current_dir)
         
     except IOError as io_error:
-        print(io_error)
+        logger.error(io_error)
         sys.exit()
     return None
 
@@ -149,19 +183,17 @@ def deploy_cc(datastore,vm_name,network1,user,password,host):
         if os.name == 'posix':
             current_dir = os.getcwd()
             os.chdir ('/usr/bin/ovftool')
-            subprocess.run(
-            f'/usr/bin/ovftool/ovftool --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"Datacenter={network1}" /home/cisco/Desktop/Lab_Build/DNAC/DNAC.ovf vi://{user}:{password}@{host}/',
-            shell=True)
+            run_subprocess_and_log(
+            f'/usr/bin/ovftool/ovftool --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"Datacenter={network1}" /home/cisco/Desktop/Lab_Build/DNAC/DNAC.ovf vi://{user}:{password}@{host}/')
             os.chdir(current_dir)
         elif os.name == 'nt':
             current_dir = os.getcwd()
             os.chdir(r'C:\Program Files\VMware\VMware OVF Tool')
-            subprocess.run(
-            f'ovftool.exe --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"Datacenter={network1}" C:/Users/admin/Desktop/Lab_Build/DNAC/DNAC1.ovf vi://{user}:{password}@{host}/',
-            shell=True)
+            run_subprocess_and_log(
+            f'ovftool.exe --powerOn -ds={datastore} -n={vm_name} -dm=thin --noSSLVerify --disableVerification --net:"Datacenter={network1}" C:/Users/admin/Desktop/Lab_Build/DNAC/DNAC1.ovf vi://{user}:{password}@{host}/')
             os.chdir(current_dir)
         
     except IOError as io_error:
-        print(io_error)
+        logger.error(io_error)
         sys.exit()
     return None
